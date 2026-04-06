c5eb933e3e572a990620b7a4b8fa9eb693815b43
Yaseenobigboi <yaseenvds@gmail.com>
2026-04-06 19:04:09 +0200

    scaffold: initialise crypto-app with Vite React and TypeScript

---
+++ crypto-app/src/App.tsx
@@ -0,0 +1,12 @@
+// this must be the root component - it will hold the router and any shared layout
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Crypto App</h1>
+    </div>
+  )
+}
+
+export default App
