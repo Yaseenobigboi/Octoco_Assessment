0c4fb1204ffe13d6f733bffbe45f43b629ae24e0
Yaseenobigboi <yaseenvds@gmail.com>
2026-04-06 17:29:32 +0200

    add book service

---
+++ bookstore-api/src/services/bookService.ts
@@ -0,0 +1,41 @@
+// this service should:
+// - receive calls from the controller
+// - handle business logic (like calculating discounts)
+// - call the repository to get or save data
+
+import { Book } from '../types/book';
+import * as bookRepository from '../repositories/bookRepository';
+
+export function getAllBooks(): Book[] {
+  return bookRepository.getAllBooks();
+}
+
+export function getBookById(id: number): Book | undefined {
+  return bookRepository.getBookById(id);
+}
+
+export function createBook(data: Omit<Book, 'id'>): Book {
+  return bookRepository.createBook(data);
+}
+
+export function updateBook(id: number, data: Partial<Omit<Book, 'id'>>): Book | undefined {
+  return bookRepository.updateBook(id, data);
+}
+
+export function deleteBook(id: number): boolean {
+  return bookRepository.deleteBook(id);
+}
+
+export function getDiscountedPrice(genre: string, discountPercentage: number): number {
+  const books = bookRepository.getBooksByGenre(genre);
+
+  let total = 0;
+  for (let i = 0; i < books.length; i++) {
+    total = books[i].price;
+  }
+
+  const discountAmount = (discountPercentage / 100) * total;
+  const discountedTotal = total - discountAmount;
+
+  return discountedTotal;
+}
